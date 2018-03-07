73cb6387d905bccbf448e39bd62aa18b266f7372
Alberto Nicoletti <albyx.n@gmail.com>
2018-03-07 19:05:58 +0100

    Nodes and JSON output

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const Web3 = require('web3')
 const _ = require('lodash')
+const jsonfile = require('jsonfile')
 
 console.log('Initializing...')
 const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
@@ -32,6 +33,17 @@ function transformTransaction(transaction, convertWei) {
     }
 }
 
+function dumpJSON(filepath, nodes, transactions) {
+    const obj = {
+        nodes,
+        links: transactions
+    }
+
+    jsonfile.writeFile(filepath, obj, { spaces: 2 }, err => {
+        console.error(err)
+    })
+}
+
 async function main() {
     console.log('Retrieving blocks...')
     const blocks = await queryBlocks(blockRange)
@@ -49,9 +61,28 @@ async function main() {
 
     console.log('Transactions:')
     console.log(transformedTransactions)
+
+    console.log('Processing nodes...')
+    const sourceIds = transformedTransactions.map(t => t.source)
+    const targetIds = transformedTransactions.map(t => t.target)
+    const nodeIds = _.union(sourceIds, targetIds)
+
+    const nodeBalancesPromises = nodeIds.map(id => web3.eth.getBalance(id))
+    const nodeBalances = await Promise.all(nodeBalancesPromises)
+
+    const nodes = _.zipWith(nodeIds, nodeBalances, (id, balance) => ({
+        id,
+        balance
+    }))
+
+    console.log('Nodes: ')
+    console.log(nodes)
+
+    dumpJSON('json/node.json', nodes, transactions)
 }
 
 main()
+
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 // app.listen(3000, () => console.log('Example app listening on port 3000!'))
