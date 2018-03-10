bc0c640a5d20d335fe458fa7d03e15fa75b6a3e8
Alberto Nicoletti <albyx.n@gmail.com>
2018-03-10 18:31:51 +0100

    Colored logging

--- src/log.js
+++ src/log.js
@@ -1,9 +1,24 @@
 const fs = require('fs')
+const colors = require('colors/safe')
 
 const { logFilename } = require('./config')
 
+const LOG = 'LOG'
+const ERROR = 'ERROR'
+
 function logDate() {
-    return new Date().toISOString()
+    return `[${new Date().toISOString()}]`
+}
+
+function uncoloredLog(level, str) {
+    const prefix = level === LOG ? 'log:   ' : 'error: '
+    return `${prefix} ${logDate()} ${str}`
+}
+
+function coloredLog(level, str) {
+    const prefix = level === LOG ? 'log:   ' : 'error: '
+    const prefixColor = level === LOG ? colors.green : colors.red
+    return `${prefixColor(prefix)} ${colors.bold(logDate())} ${str}`
 }
 
 function createLogStream(path) {
@@ -14,14 +29,12 @@ function createLogger(path) {
     const stream = createLogStream(path)
     return {
         log: str => {
-            const logStr = `${logDate()} - log - ${str}`
-            console.log(logStr)
-            stream.write(logStr + '\n')
+            console.log(coloredLog(LOG, str))
+            stream.write(uncoloredLog(LOG, str) + '\n')
         },
         error: str => {
-            const logStr = `${logDate()} - error - ${str}`
-            console.error(logStr)
-            stream.write(logStr + '\n')
+            console.log(coloredLog(ERROR, str))
+            stream.write(uncoloredLog(ERROR, str) + '\n')
         },
         end: () => {
             stream.end()
